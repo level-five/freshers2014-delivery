f33b5d195fbc0dbd4c836ca2c7f98d417f8f415a
satoh-disk <daisuke.satou@level-five.jp>
2014-04-09 16:10:25 +0900

    配送方法テストにてchar型記号を渡した場合は例外が発生するテスト追加。

--- src/test/java/jp/level_five/freshers/delivery/ClientDataTest.java
+++ src/test/java/jp/level_five/freshers/delivery/ClientDataTest.java
@@ -4,33 +4,38 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.omg.CORBA.PUBLIC_MEMBER;
 
 public class ClientDataTest {
 	ClientData clientData = null;
+
 	@Before
-	public void before(){
+	public void before() {
 		clientData = new ClientData();
 	}
-	
+
 	@Test
-	public void testchar型AをcheckDataOfArrivalに渡すと例外が発生しない() {	
+	public void testchar型AをcheckDataOfArrivalに渡すと例外が発生しない() {
 		clientData.checkDataOfArrival('A');
 	}
-	
-	@Test(expected=RuntimeException.class)
+
+	@Test(expected = RuntimeException.class)
 	public void testchar型XをcheckDataOfArrivalに渡すと例外が発生する() {
 		clientData.checkDataOfArrival('X');
 	}
-	
+
 	@Test
-	public void testchar型bをcheckDataOfArrivalに渡すと例外が発生しない(){
+	public void testchar型bをcheckDataOfArrivalに渡すと例外が発生しない() {
 		clientData.checkDataOfArrival('b');
 	}
-	
-	@Test(expected=RuntimeException.class)
-	public void testchar型1をcheckDataOfArrivalに渡すと例外が発生する(){
+
+	@Test(expected = RuntimeException.class)
+	public void testchar型1をcheckDataOfArrivalに渡すと例外が発生する() {
 		clientData.checkDataOfArrival('1');
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testchar型アンドをcheckDataOfArrivalに渡すと例外が発生する() {
+		clientData.checkDataOfArrival('＆');
 	}
 
 }
