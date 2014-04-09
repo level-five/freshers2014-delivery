5ccb7f191a2daa8cfac5b345171dedd092f1f78a
TakahikoYoshii <r.yoshii.n@gmail.com>
2014-04-09 18:46:00 +0900

    実装した内容 １　入力した希望日（午前）から２パターンの発送方法を文字列型に変換できるようにした。
    実装中のメソッド
    １　preSendShippingDate

--- src/main/java/jp/level_five/freshers/delivery/ShippingDateByServiceA.java
+++ src/main/java/jp/level_five/freshers/delivery/ShippingDateByServiceA.java
@@ -2,13 +2,38 @@ package jp.level_five.freshers.delivery;
 
 public class ShippingDateByServiceA {
 
-	public StringBuilder preSendShippingDate(int i, int j, String string) {
+	public StringBuilder preSendShippingDate(int postalCode, int arrivalDate,
+			String string) {
+		int am;
+		int pm;
 		StringBuilder sb = new StringBuilder();
-		if(i<8){
-			sb.append("２０１４年４月８日午前、２０１４年４月８日午後");
-		}else if(i==8){
-			sb.append("２０１４年４月８日午前、２０１４年４月７日午後");
-		}else{
+
+		if (postalCode < 8) {
+			am = --arrivalDate;
+			if (string == "午前") {
+				pm = --arrivalDate;
+			} else {
+				pm = am;
+			}
+			StringBuilder amString = new StringBuilder(Integer.toString(am));
+			StringBuilder pmString = new StringBuilder(Integer.toString(pm));
+			
+			String year = amString.substring(0,4) + "年";
+			String month = amString.substring(4,6)+"月";
+			String day = amString.substring(6)+"日";
+			
+			sb.append(year + month + day + "午前" + "、");
+			
+			year = pmString.substring(0,4) + "年";
+			month = pmString.substring(4,6)+"月";
+			day = pmString.substring(6)+"日";
+			
+			sb.append(year + month + day + "午後");
+			System.out.println(sb);
+			
+		} else if (postalCode == 8) {
+//			sb.append();
+		} else {
 			sb.append("配送不可");
 		}
 		return sb;
--- src/test/java/jp/level_five/freshers/delivery/ShippingDateByServiceATest.java
+++ src/test/java/jp/level_five/freshers/delivery/ShippingDateByServiceATest.java
@@ -6,33 +6,45 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ShippingDateByServiceATest {
-	ShippingDateByServiceA service ;
+	ShippingDateByServiceA service;
+
 	@Before
-	public void setUp(){service = new ShippingDateByServiceA();}
-
-
+	public void setUp() {
+		service = new ShippingDateByServiceA();
+	}
+//
+//	@Test
+//	public void test郵便番号が１で２０１４年４月９日夕方が到着日の時を渡すとStringBuilder型で発送日が返ってくる() {
+//		StringBuilder sb = service.preSendShippingDate(1, 20140409, "夕方");
+//		String str = "2014年4月8日午前、2014年4月8日午後";
+//		assertEquals(str, sb.toString());
+//	}
+//
+//	@Test
+//	public void test郵便番号が８で２０１４年4月9日午後が到着日の時を渡すとStringBuilder型で発送日が返ってくる() {
+//		StringBuilder sb = service.preSendShippingDate(8, 20140409, "午後");
+//		String str = "2014年4月8日午前、2014年4月7日午後";
+//		assertEquals(str, sb.toString());
+//	}
+//
+//	@Test
+//	public void test郵便番号が９で２０１４年4月9日午後が到着日の時を渡すとStringBuilder型で発送日が返ってくる() {
+//		StringBuilder sb = service.preSendShippingDate(9, 20140409, "午後");
+//		String str = "配送不可";
+//		assertEquals(str, sb.toString());
+//	}
+//
+//	@Test
+//	public void test郵便番号が３で２０１４年4月9日午後が到着日の時を渡すとStringBuilder型で発送日が返ってくる() {
+//		StringBuilder sb = service.preSendShippingDate(3, 20140409, "午後");
+//		String str = "2014年4月8日午前、2014年4月8日午後";
+//		assertEquals(str, sb.toString());
+//	}
+	
 	@Test
-	public void test郵便番号が１で２０１４年４月９日夕方が到着日の時を渡すとStringBuilder型で発送日が返ってくる() {
-		StringBuilder sb = service.preSendShippingDate(1, 20140409, "夕方");
-		String str ="２０１４年４月８日午前、２０１４年４月８日午後";
+	public void test郵便番号が1で２０１４年4月9日午前が到着日の時を渡すと20140407午後と20140408午前で発送日が返ってくる() {
+		StringBuilder sb = service.preSendShippingDate(1, 20140409, "午前");
+		String str = "2014年04月08日午前、2014年04月07日午後";
 		assertEquals(str, sb.toString());
 	}
-	@Test
-	public void test郵便番号が８で２０１４年4月9日午後が到着日の時を渡すとStringBuilder型で発送日が返ってくる(){
-		StringBuilder sb = service.preSendShippingDate(8, 20140409, "午後");
-		String str ="２０１４年４月８日午前、２０１４年４月７日午後";
-		assertEquals(str, sb.toString());
-	}
-	@Test
-	public void test郵便番号が９で２０１４年4月9日午後が到着日の時を渡すとStringBuilder型で発送日が返ってくる(){
-		StringBuilder sb = service.preSendShippingDate(9, 20140409, "午後");
-		String str = "配送不可";
-		assertEquals(str, sb.toString());
-	}
-	@Test
-	public void test郵便番号が３で２０１４年4月9日午後が到着日の時を渡すとStringBuilder型で発送日が返ってくる(){
-		StringBuilder sb = service.preSendShippingDate(3, 20140409, "午後");
-		String str ="２０１４年４月８日午前、２０１４年４月８日午後";
-		assertEquals(str, sb.toString());
-	}
-	}
+}
