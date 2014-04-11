3d88678ed79c2765c71a4e0daeb182946dd290c3
yuich2014 <kawashima@level-five.jp>
2014-04-11 11:51:24 +0900

    switch文にbreakが不足していたのを修正
    isHolidayメソッドのリファクタリング

--- src/main/java/jp/level_five/freshers/delivery/ShippingDataByServiceB.java
+++ src/main/java/jp/level_five/freshers/delivery/ShippingDataByServiceB.java
@@ -6,63 +6,85 @@ public class ShippingDataByServiceB {
 	Calendar calendar = Calendar.getInstance();
 
 	public boolean isHoliday(int dataOfArrival) {
-		int year = Integer.parseInt(String.valueOf(dataOfArrival).substring(0,
-				4));
-		int month = Integer.parseInt(String.valueOf(dataOfArrival).substring(4,
-				6)) - 1;
-		int day = Integer.parseInt(String.valueOf(dataOfArrival)
-				.substring(6, 8));
+		int year = Integer.parseInt(String.valueOf(dataOfArrival).substring(0, 4));
+		int month = Integer.parseInt(String.valueOf(dataOfArrival).substring(4,	6)) - 1;
+		int day = Integer.parseInt(String.valueOf(dataOfArrival).substring(6, 8));
 
 		calendar.set(year, month, day);
-		int week = calendar.get(Calendar.DAY_OF_WEEK);
+		int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
 
-		int week1 = calendar.get(Calendar.DAY_OF_WEEK_IN_MONTH);
+		int dayOfWeekInMonth = calendar.get(Calendar.DAY_OF_WEEK_IN_MONTH);
 
-		if (week == 1) {
+		if (dayOfWeek == Calendar.SUNDAY) {
 			return true;
 		} else {
 			switch (month) {
-			case 0:
+			case Calendar.JANUARY:
 				if (day == 1)
 					return true;
-				if (week == 2 && week1 == 2)
+				if (dayOfWeek == Calendar.MONDAY && dayOfWeekInMonth == 2)
 					return true;
-
-			case 1:
+				break;
+				
+			case Calendar.FEBRUARY:
 				if (day == 11)
 					return true;
-			case 2:
+				break;
+				
+			case Calendar.MARCH:
 				if (day == 21)
 					return true;
-			case 3:
+				break;
+				
+			case Calendar.APRIL:
 				if (day == 29)
 					return true;
-				
-			case 4:
+				break;
+
+			case Calendar.MAY:
 				if (day == 3)
 					return true;
 				if (day == 4)
 					return true;
 				if (day == 5)
 					return true;
-			case 6:
-				if (week == 2 && week1 == 3)
+				break;
+				
+			case Calendar.JUNE:
+				break;
+				
+			case Calendar.JULY:
+				if (dayOfWeek == Calendar.MONDAY && dayOfWeekInMonth == 3)
 					return true;
-			case 8:
-				if (week == 2 && week1 == 3)
+				break;
+			case Calendar.AUGUST:
+				break;
+				
+			case Calendar.SEPTEMBER:
+				if (day == 23)
 					return true;
-			case 9:
-				if (week == 2 && week1 == 2)
+				if (dayOfWeek == Calendar.MONDAY && dayOfWeekInMonth == 3)
 					return true;
-			case 10:
+				break;
+				
+			case Calendar.OCTOBER:
+				if (dayOfWeek == Calendar.MONDAY && dayOfWeekInMonth == 2)
+					return true;
+				break;
+				
+			case Calendar.NOVEMBER:
 				if (day == 3)
 					return true;
 				if (day == 23)
 					return true;
-			case 11:
+				break;
+				
+			case Calendar.DECEMBER:
 				if (day == 23)
 					return true;
+				break;
 			}
+			
 			return false;
 		}
 	}
