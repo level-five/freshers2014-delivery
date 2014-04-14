34566b71c967bf47b9b2dc28b0877bce0bc1b596
TakahikoYoshii <r.yoshii.n@gmail.com>
2014-04-14 13:37:49 +0900

    動作テスト用のMainクラスの作成

---
+++ src/main/java/jp/level_five/freshers/delivery/Main.java
@@ -0,0 +1,8 @@
+package jp.level_five.freshers.delivery;
+
+public class Main {
+	public static void main(String[] aargs){
+		InputOutput inputoutput = new InputOutput();
+		inputoutput.inputMode();
+	}
+}
