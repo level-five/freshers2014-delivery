2a670f7e971dcfb73aca33bd50fa3c26743e36e8
andohmikihiro <andoh@level-five.jp>
2014-04-16 10:42:48 +0900

    Mainのクラスを変更して全機能を実現

--- src/main/java/jp/level_five/freshers/delivery/Main.java
+++ src/main/java/jp/level_five/freshers/delivery/Main.java
@@ -3,6 +3,6 @@ package jp.level_five.freshers.delivery;
 public class Main {
 	public static void main(String[] aargs){
 		InputOutput inputoutput = new InputOutput();
-		inputoutput.inputMode();
+		inputoutput.modeSelect();
 	}
 }
